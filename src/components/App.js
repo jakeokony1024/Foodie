0296141c9e4a01e1493015c1557dc638073c96fd
Jakeokony1024 <jakeokony@gmail.com>
2022-02-21 22:38:53 -0500

    header component created

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from './Header';
+
+const App = () => {
+ return (
+  <div className="ui container">
+   <Header/>
+  </div>
+ )
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+ return (
+  <div className="ui secondary pointing menu">
+   <div className="left menu">
+     Foodie
+   </div>
+   <div className="right menu">
+     Login / Sign Up
+   </div>
+  </div>
+ )
+}
+
+export default Header;
