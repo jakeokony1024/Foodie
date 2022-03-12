96765459f5b82c9afca7b67845dbf2ace632e8a7
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2022-03-12 10:38:55 -0500

    Implementation of React Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render
 (
- <App/>,
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
  document.querySelector('#root')
 )
