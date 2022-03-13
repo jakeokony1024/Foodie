a9539e416512366e6f907fe6a064a430a2a88f63
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2022-03-13 11:48:26 -0400

    Created Login constants

---
+++ src/containers/App/constants.js
@@ -0,0 +1,3 @@
+export const LOGIN_USER = 'foodie/App/LOGIN_USER';
+export const LOGIN_USER_SUCCESS = 'foodie/App/LOGIN_USER_SUCCESS';
+export const LOGIN_USER_ERROR = 'foodie/App/LOGIN_USER_ERROR';
