1b0cc040751a249c1891e5342be8c3a701803318
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2022-03-13 11:49:42 -0400

    Changes to App Index for Store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import FontFaceObserver from 'fontfaceobserver';
+import 'sanitize.css/sanitize.css';
 
-import App from './App';
+// Import root app
+import App from './containers/App';
+
+
+import configureStore from './configureStore';
+
 import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render
-(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
- document.querySelector('#root')
-)
+
+// Create redux store with history
+const initialState = {};
+const store = configureStore(initialState);
+const MOUNT_NODE = document.getElementById('app');
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
+    MOUNT_NODE,
+);
