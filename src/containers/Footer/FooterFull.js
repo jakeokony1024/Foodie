ab94db41bde9dadf231a9601759506ca8b16489f
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2022-03-20 15:38:53 -0400

    Package Changes

---
+++ src/containers/Footer/FooterFull.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FooterFull = (props) => {
+    return (
+        <div className='footer footer-full'>
+            <span>Foodie Technologies Inc. &copy; 2022 All Rights Reserved.</span>
+            <a href="">Terms & Conditions</a>
+            <a href="">Privacy Policy</a>
+            <a href="">Contact Support</a>
+        </div>
+    );
+}
+
+export default FooterFull;
