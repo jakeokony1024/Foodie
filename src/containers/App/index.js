5059d542fd8d19d739c202bef0521dcd7cff9f1c
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2022-03-30 21:26:34 -0400

    Update index.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,22 +1,30 @@
 import TopBannerNotification from "../../components/TopBannerNotification";
-import {Route, Routes} from "react-router-dom";
+import {Route, Router, Switch} from 'react-router-dom';
 import WelcomePage from "../WelcomePage";
 import FeedPage from "../FeedPage";
 import React from "react";
 import '../../components/Css/index.scss';
+import ProfilePage from "../ProfilePage";
 
 export default function App() {
+
+
     return (
         <div className='main-wrapper'>
             <TopBannerNotification forceHide={false}/>
-            <Routes>
-                {/*authentication*/}
-                <Route path="/" element={<WelcomePage />} />
-                <Route path="/feed" element={<FeedPage />} />
-                <Route path="login" element={<WelcomePage />} />
-                <Route path="signup" element={<WelcomePage />} />
-                <Route path="forgot-password" element={<WelcomePage />} />
-            </Routes>
+            <Switch>
+                <Route exact path="/">
+                    <WelcomePage />
+                </Route>
+
+                <Route path="/feed">
+                    <FeedPage />
+                </Route>
+
+                <Route path="/profile">
+                    <ProfilePage />
+                </Route>
+            </Switch>
         </div>
     )
 }
