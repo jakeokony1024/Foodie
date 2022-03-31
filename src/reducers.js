064d9a93d76c261b2ddf90389aae3d3c2978efb4
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2022-03-30 21:26:57 -0400

    Update reducers.js

--- src/reducers.js
+++ src/reducers.js
@@ -3,9 +3,10 @@
  */
 
 import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router';
-
+import history from './utils/history.js';
 import globalReducer from './containers/App/reducer.js';
+import {connectRouter} from "connected-react-router";
+import {api} from "./services/api";
 
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
@@ -13,7 +14,10 @@ import globalReducer from './containers/App/reducer.js';
 export default function createReducer(injectedReducers = {}) {
     const rootReducer = combineReducers({
         global: globalReducer,
+        router: connectRouter(history),
+        [api.reducerPath]: api.reducer,
         ...injectedReducers,
+
     });
 
     return rootReducer;
